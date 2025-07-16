a6dfefa116c3bfd557b88cbf75b66fdfe9eedb9d
LeeHyunWoo <hyoun0824@naver.com>
2025-07-17 01:07:49 +0900

    docs : 기본설정

---
+++ src/main/java/Project/PENBOT/PenbotApplication.java
@@ -0,0 +1,13 @@
+package Project.PENBOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Project/PENBOT/PenbotApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.PENBOT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
