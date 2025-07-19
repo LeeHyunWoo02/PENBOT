c68316e1695ff0094352c0d22d62de9d80794542
LeeHyunWoo <hyoun0824@naver.com>
2025-07-19 21:18:03 +0900

    feat : Verify Controller 작성

--- src/main/java/Project/PENBOT/Verify/Controller/VerfiyController.java
+++ src/main/java/Project/PENBOT/Verify/Controller/VerfiyController.java
@@ -2,12 +2,16 @@ package Project.PENBOT.Verify.Controller;
 
 import Project.PENBOT.Verify.Dto.SmsRequestDto;
 import Project.PENBOT.Verify.Dto.SmsResponseDto;
+import Project.PENBOT.Verify.Dto.VerifyRequestDto;
 import Project.PENBOT.Verify.Service.VerifyService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/verify")
 public class VerfiyController {
     private final VerifyService verifyService;
 
@@ -15,6 +19,7 @@ public class VerfiyController {
         this.verifyService = verifyService;
     }
 
+    @PostMapping("/sendcode")
     private ResponseEntity<SmsResponseDto> sendAuthCode(@RequestBody SmsRequestDto request) {
         String toPhone = request.getToPhone();
         if( verifyService.sendAuthCodeAndSave(toPhone)){
@@ -25,5 +30,21 @@ public class VerfiyController {
         return ResponseEntity.status(500).body(
                 new SmsResponseDto(false, "인증번호 전송에 실패했습니다. 다시 시도해주세요.")
         );
+    }
+
+    @PostMapping("/verifycode")
+    private ResponseEntity<SmsResponseDto> sendSmsRequest(@RequestBody VerifyRequestDto request) {
+        String phone = request.getPhone();
+        String code = request.getCode();
+
+        if (verifyService.verifyCode(phone, code)) {
+            return ResponseEntity.ok(
+                    new SmsResponseDto(true, "인증번호가 일치합니다.")
+            );
+        } else {
+            return ResponseEntity.status(400).body(
+                    new SmsResponseDto(false, "인증번호가 일치하지 않습니다.")
+            );
+        }
     }
 }
